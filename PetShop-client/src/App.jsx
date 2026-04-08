abdf2ef9379689f6fd5ffa08065cdccb6068a4e7
phucphan03 <dinhphucqn44@gmail.com>
2026-04-08 14:41:41 +0700

    API - client

---
+++ PetShop-client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-red">Hello PetShop 🐶</h1>
+      <button className="btn btn-success">Click me</button>
+      <h1 className="text-xl-end">fsdf</h1>
+    </div>
+  )
+}
+
+export default App
